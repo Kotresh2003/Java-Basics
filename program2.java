a9792db17d6c98bba787b60c04bd35399a3b1e2b
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-11 00:09:46 +0530

    java2

---
+++ program2.java
@@ -0,0 +1,12 @@
+public class program2 {
+    public static void main (String [] args){
+       System.out.println("introduction");
+       System.out.println(143);
+       System.out.println(4.55);
+       System.out.println('M');
+       System.out.println(true);
+       System.out.println(false);
+       System.out.println('$');
+       System.out.println("$_@#$%");
+    }
+}
---
+++ program3.java
@@ -0,0 +1,9 @@
+public class program3 {
+   public static void main (String [] args){
+     System.out.println("kottresh");
+       System.out.println(2025);
+       System.out.println(8);
+       System.out.println('M');
+       System.out.println(true);
+   } 
+}
