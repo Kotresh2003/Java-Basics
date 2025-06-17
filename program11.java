0d1eccea410fac59a1bb2348203f51556a70c55c
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-18 02:04:10 +0530

    learn

---
+++ program11.java
@@ -0,0 +1,16 @@
+class program11 {
+public static void main(String[] args)
+{
+    int age;
+    age=22;
+    System.out.println(age);
+
+    double cgpa;
+    cgpa=8.98;
+    System.out.println(cgpa);
+
+    boolean love;
+    love=true;
+    System.out.println(love);
+}    
+}
---
+++ program12.java
@@ -0,0 +1,13 @@
+class program12 {
+    public static void main(String[] args)
+    {
+        int age=22;
+        System.out.println(age);
+
+        double cgpa=7.88;
+        System.out.println(cgpa);
+
+        boolean love=false;
+        System.out.println(love);
+    }
+}
