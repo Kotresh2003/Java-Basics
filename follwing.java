4f7be43737c896f4e37a74182e5deb70e93ac0e5
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-18 23:23:11 +0530

    array

---
+++ follwing.java
@@ -0,0 +1,11 @@
+public class follwing {
+    public static void main(String[] args){
+
+        int ariiy[] = {1,2,3,4,5,6,7,8,9,10,11,47,14,52,65,74,85,24,15};
+        int n=ariiy.length;
+        for (int i =0; i < n; i++){
+        System.out.print(ariiy[i] + " ");    
+        }
+        
+    }   
+}
