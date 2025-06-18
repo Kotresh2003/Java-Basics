baea48fbc2d4ec2ca266d90909f8e0d1a8d7233d
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-19 00:14:57 +0530

    array string

--- follwing.java
+++ follwing.java
@@ -3,9 +3,9 @@ public class follwing {
 
         int ariiy[] = {1,2,3,4,5,6,7,8,9,10,11,47,14,52,65,74,85,24,15};
         int n=ariiy.length;
-        for (int i =0; i < n; i++){
-        System.out.print(ariiy[i] + " ");    
-        }
-        
+        // for (int i =0; i < n; i++){
+        // System.out.print(ariiy[i] + " ");    
+        // }
+        System.out.println(ariiy[10]);
     }   
 }
---
+++ like.java
@@ -0,0 +1,16 @@
+public class like {
+    public static void main(String[] args)
+    {
+        int[] arr=new int[6];
+
+        arr [0]=10;
+        arr [1]=20;
+        arr [2]=30;
+        arr [3]=40;
+        arr [4]=50;
+        arr [5]=100;
+
+        for(int i =0; i<arr.length; i++)
+        System.out.println("index of element"+i+ " = " +arr[i]);
+    }
+}
