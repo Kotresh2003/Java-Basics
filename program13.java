b1beeb3faa625caa9de9f4dd29cf7ee7eabcd43a
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-19 20:32:28 +0530

    operators

---
+++ program13.java
@@ -0,0 +1,15 @@
+public class program13 {
+    public static void main(String[] args)
+    {
+        int a=34;
+        int b=44;
+        int c=50;
+
+        System.out.println(a+b-c);
+        System.out.println(a-b/c);
+        System.out.println(a*b-a);
+        System.out.println(a/b+c);
+
+
+    }
+}
