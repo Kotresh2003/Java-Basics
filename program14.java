fc9bcf91ab2f12f6fe873d92e03b968fd467d93c
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-19 20:41:31 +0530

    decimal

---
+++ program14.java
@@ -0,0 +1,13 @@
+public class program14 {
+    public static void main(String[] args)
+    {
+        int p=30;
+        int q=87;
+
+        double x=6.76;
+        double y=7.99;
+
+        System.out.println(p/q);
+        System.out.println(x/y);
+    }
+}
