b8cca586220833f95bec037ba7d3b99a9afac1ff
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-19 20:56:22 +0530

    cube and square

---
+++ program15.java
@@ -0,0 +1,12 @@
+public class program15 {
+    public static void main(String[] args)
+{
+    int n=3;
+    int cube=n*n*n;
+
+    System.out.println(n+"cube is :"+cube);
+    // int n=4;
+    // int square=n*n;
+    // System.out.println(n+"square is :"+square);
+}
+}
