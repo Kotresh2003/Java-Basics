729716be9211894388ee905975900ca668aba3f2
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-19 21:11:44 +0530

    area and radius

---
+++ program16.java
@@ -0,0 +1,13 @@
+public class program16 {
+    public static void main(String[] args)
+    {
+        double pi=3.14;
+        double radius=2.14;
+
+        double area=pi*radius*radius;
+       double circumference=2*radius;
+
+        System.out.println("area of circle:"+ area);
+        System.out.println("circumference of circle :"+circumference);
+    }
+}
