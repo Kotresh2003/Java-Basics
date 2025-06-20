81522d33709ff4411a73975d00abb48be9fc0fb7
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-20 23:48:16 +0530

    elseif

---
+++ program25.java
@@ -0,0 +1,18 @@
+public class program25 {
+    public static void main(String[] args)
+    {
+        int age = 31;
+        if(age>50){
+            System.out.println("you are experienced");
+        }
+        else if(age>40){
+            System.out.println("yor are semi exoerinced");
+        }
+        else if(age>30){
+            System.out.println("you are semi semi experinced");
+        }
+        else{
+            System.out.println("you are not experienced");
+        }
+    }
+}
