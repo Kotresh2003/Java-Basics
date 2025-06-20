ff3b67793311fb6d99d64ade82175bf5b26d3c7d
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-21 00:11:13 +0530

    switch case elseif

--- program25.java
+++ program25.java
@@ -1,18 +1,45 @@
+import java.util.Scanner;
 public class program25 {
+    
     public static void main(String[] args)
     {
-        int age = 31;
-        if(age>50){
-            System.out.println("you are experienced");
+        int age;
+        System.out.println("enter your age");
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+
+        switch(age){
+            case 18:
+            System.out.println("you are adult");
+           break;
+    
+            case 25:
+            System.out.println("you are yet to married");
+            break;
+
+            case 60:
+            System.out.println("you are old");
+           break;
+            
+            default:
+            System.out.println("enjoy your life");
+
         }
-        else if(age>40){
-            System.out.println("yor are semi exoerinced");
-        }
-        else if(age>30){
-            System.out.println("you are semi semi experinced");
-        }
-        else{
-            System.out.println("you are not experienced");
-        }
-    }
-}
+//         if (age>56){
+//             System.out.println("yor are experienced");
+//         }
+//         // int age = 31;
+//         if(age>50){
+//             System.out.println("you are experienced");
+//         }
+//         else if(age>40){
+//             System.out.println("yor are semi exoerinced");
+//         }
+//         else if(age>30){
+//             System.out.println("you are semi semi experinced");
+//         }
+//         else{
+//             System.out.println("you are not experienced");
+//         }
+     }
+ }
