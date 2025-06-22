c69812eaa7d926c671c15c45c52995e9f00147fd
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-23 02:07:00 +0530

    while and do while

---
+++ program31.java
@@ -0,0 +1,27 @@
+public class program31 {
+    public static void main(String[] args)
+    {
+        //while loops
+        int i =5;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        while (true) {
+            System.out.println("i am kotresh");
+         }
+  
+     //do while loops
+        int a =1;
+        do {
+            System.out.println(a);
+            a++;
+        }while(a<=5);
+
+        int b=1;
+        do {
+            System.out.println(b);
+            b++;
+        }while(b<45);
+    }
+}
