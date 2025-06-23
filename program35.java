dd2a4817841d2d3b177081d7162f829ce4cf7e4f
Kotresh KV <kotreshkotresh1562@example.com>
2025-06-24 00:15:00 +0530

    array and reverse order

---
+++ program35.java
@@ -0,0 +1,17 @@
+class program35{
+    public static void main(String[] args)
+    {
+        // int [] marks; //decleration and memory allocation
+        // marks = new int[5];
+        // marks[0] = 100;// initialization
+        // marks[1] = 86;
+        // marks[2] = 70;
+        // marks[3] = 90;
+        // marks[4] = 65;
+        // System.out.println(marks[3]);
+
+
+        int [] marks = {100, 86, 70, 90, 65};
+        System.out.println(marks[0]);
+    }
+}
